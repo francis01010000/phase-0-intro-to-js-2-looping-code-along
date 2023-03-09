6b000a9c072b665576d3e3f7e53e0168b1801ee6
Francisco Perez <Franciscop693@gmail.com>
2023-03-09 00:23:37 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const message = [];
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return message;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }
+    return number;
+}
+
+countDown(number)
